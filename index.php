7fc9ad321ea6129f99185dc73446e8839320b54f
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-02 16:46:27 +0200

    add censoredText

--- index.php
+++ index.php
@@ -23,6 +23,19 @@ $text= 'Ho un cucciolo di Shiba Inu di nome Yuki e oggi fa 4 mesi!';
 
 $lenghtText = strlen($text);
 
+
+
+
+$censored = $_GET['censored'];
+
+
+
+$textCensored = str_replace($censored, '***', $text);
+
+
+
+$lenghtCensoredText = strlen($textCensored);
+
 ?>
 
 
@@ -30,3 +43,14 @@ $lenghtText = strlen($text);
 
 
 <h3>La lunghezza del testo è di <?php echo $lenghtText ?> caratteri</h3>
+
+
+
+<h2>La frase modificata:</h2>
+<p>
+    <?php echo $textCensored?>
+</p>
+<p>
+    La lunghezza del testo modificato è di: <?php echo $lenghtCensoredText ?> caratteri
+
+</p>
